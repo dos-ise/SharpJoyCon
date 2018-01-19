174fbc1f2b1f3b6647391a1591ed89b428ee2e43
Dennis Ostermann <dennis.ostermann@ise.de>
2018-01-19 12:04:27 +0100

    Added log4net to consoledemo

--- SharpJoyConConsoleDemo/Program.cs
+++ SharpJoyConConsoleDemo/Program.cs
@@ -5,10 +5,20 @@ using System.Threading;
 
 namespace SharpJoyConConsoleDemo
 {
+  using System.IO;
+  using System.Reflection;
+  using System.Text;
+  using System.Xml;
+  using System.Xml.Linq;
+
+  using log4net;
+  using log4net.Config;
+
   public class Program
   {
     static void Main(string[] args)
     {
+      SetupLog4Net();
       var manager = new JoyconManager();
       manager.ConnectJoyCons();
       manager.Start();
@@ -26,6 +36,29 @@ namespace SharpJoyConConsoleDemo
       }
 
       manager.DisconnectJoyCons();
+    }
+
+    private static void SetupLog4Net()
+    {
+      var assembly = Assembly.GetExecutingAssembly();
+      var logConfigName = assembly.GetManifestResourceNames().SingleOrDefault(me => me.Contains("log4net"));
+      if (logConfigName != null)
+      {
+        Stream resourceStream = assembly.GetManifestResourceStream(logConfigName);
+        using (var reader = new StreamReader(resourceStream, Encoding.UTF8))
+        {
+          string logConfig = reader.ReadToEnd();
+          var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+          XmlConfigurator.Configure(logRepository, ToXmlElement(XElement.Parse(logConfig)));
+        }
+      }
+    }
+
+    private static XmlElement ToXmlElement(XElement el)
+    {
+      var doc = new XmlDocument();
+      doc.Load(el.CreateReader());
+      return doc.DocumentElement;
     }
   }
 }
